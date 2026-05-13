99f7e6a09484d57069c0f247b3eec1e1d743b704
Abdirahman <abdirahmaanqaliif3@gmail.com>
2026-05-13 22:47:33 +0200

    Lade till Selenium tester

---
+++ tests/browser.test.js
@@ -0,0 +1,32 @@
+const { Builder, By, until } = require('selenium-webdriver');
+require('geckodriver');
+
+const fileUnderTest = 'file://' + __dirname.replaceAll(/ /g, '%20').replaceAll(/\\/g, '/') + '/../dist/index.html';
+const defaultTimeout = 10000;
+let driver;
+jest.setTimeout(1000 * 60 * 5);
+
+beforeAll(async () => {
+    console.log(fileUnderTest);
+    driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(fileUnderTest);
+});
+
+afterAll(async() => {
+    await driver.quit();
+}, defaultTimeout);
+
+test('The stack should be empty in the beginning', async () => {
+    let stack = await driver.findElement(By.id('top_of_stack')).getText();
+    expect(stack).toEqual("n/a");
+});
+
+describe('Clicking "Pusha till stacken"', () => {
+    it('should open a prompt box', async () => {
+        let push = await driver.findElement(By.id('push'));
+        await push.click();
+        let alert = await driver.switchTo().alert();
+        await alert.sendKeys("Bananer");
+        await alert.accept();
+    });
+});
